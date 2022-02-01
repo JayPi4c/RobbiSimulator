7f7890510f9c0cec8d66c73c4e6c7bc448f2490a
Jonas Pohl <jonas.jp4c@gmail.com>
2022-02-01 19:07:38 +0100

    Adding autogenerated properties file.
    If a properties-file can be found data will be loaded from the file
    otherwise default entries will be added to the properties object which
    will be saved at program termination.
    This fixes #43

--- src/main/java/com/JayPi4c/RobbiSimulator/utils/PropertiesLoader.java
+++ src/main/java/com/JayPi4c/RobbiSimulator/utils/PropertiesLoader.java
@@ -33,6 +33,9 @@ public class PropertiesLoader {
 	/**
 	 * Loads the properties and stores them in an Object.
 	 * 
+	 * If the simulator.properties file can't be found, default values will be
+	 * loaded.
+	 * 
 	 * @return true if the initialization was successful, false otherwise
 	 */
 	public static boolean initialize() {
@@ -41,6 +44,7 @@ public class PropertiesLoader {
 			properties.load(in);
 			return true;
 		} catch (IOException e) {
+			loadDefaultProperties();
 			return false;
 		}
 	}
@@ -52,8 +56,9 @@ public class PropertiesLoader {
 	 */
 	public static boolean isTutor() {
 		try {
-			return properties.getProperty("role").equalsIgnoreCase("Tutor");
+			return properties.getProperty("role").equalsIgnoreCase("tutor");
 		} catch (NullPointerException e) {
+			properties.put("role", "student");
 			return false;
 		}
 	}
@@ -65,11 +70,11 @@ public class PropertiesLoader {
 	 *         propterty is found
 	 */
 	public static String getTutorhost() {
-		try {
-			return properties.getProperty("tutorhost");
-		} catch (NullPointerException e) {
-			return "localhost";
-		}
+		String key = "tutorhost";
+		String host = properties.getProperty(key);
+		if (host == null)
+			properties.put(key, host = "localhost");
+		return host;
 	}
 
 	/**
@@ -81,7 +86,8 @@ public class PropertiesLoader {
 		try {
 			return Integer.parseInt(properties.getProperty("tutorport"));
 		} catch (NumberFormatException | NullPointerException e) {
-			return -1;
+			properties.put("tutorport", "3579");
+			return 3579;
 		}
 	}
 
@@ -96,6 +102,7 @@ public class PropertiesLoader {
 			return new Locale(parts[0], parts[1]);
 		} catch (IndexOutOfBoundsException | NullPointerException e) {
 			logger.debug("Failed to load locale from properties");
+			properties.put("lang", Locale.GERMANY.toString());
 			return Locale.GERMANY;
 		}
 	}
@@ -113,6 +120,17 @@ public class PropertiesLoader {
 		} catch (IOException e) {
 			return false;
 		}
+	}
+
+	/**
+	 * Loads initial values in the properties object. This is needed if the
+	 * application failed to load properties from the file.
+	 */
+	private static void loadDefaultProperties() {
+		properties.put("lang", Locale.GERMANY.toString());
+		properties.put("role", "student");
+		properties.put("tutorport", "3579");
+		properties.put("tutorhost", "localhost");
 	}
 
 }
