7ffbe670b62195e9c4342c555f00c2e944fc7ee3
Jonas Pohl <jonas.jp4c@gmail.com>
2022-04-05 00:00:57 +0200

    Adding tests

--- src/test/java/com/JayPi4c/RobbiSimulator/model/TerritoryTest.java
+++ src/test/java/com/JayPi4c/RobbiSimulator/model/TerritoryTest.java
@@ -2,7 +2,9 @@ package com.JayPi4c.RobbiSimulator.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
@@ -12,8 +14,39 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class TerritoryTest {
+	
 	@Mock
 	TerritoryState stateMock;
+
+	@Mock
+	Robbi robbiMocK;
+
+	@Mock
+	Territory territoryMock;
+
+
+	@Test
+	void testConstructor() {
+		Territory territory = new Territory();
+		assertNotNull(territory);
+		assertNotNull(territory.getRobbi());
+		assertNotNull(territory.getTile(0, 0));
+		assertNotNull(territory.getTile(territory.getNumCols(), territory.getNumRows()));
+	}
+
+	@Test
+	void testSave() {
+		Territory territory = new Territory();
+		territory.changeSize(4, 2);
+		territory.placeAccu(0, 0);
+
+		TerritoryState state = territory.save();
+
+		assertNotNull(state);
+		assertEquals(4, state.getNumberOfColumns());
+		assertEquals(2, state.getNumberOfRows());
+		assertTrue(state.getTiles()[0][0].getItem() instanceof Accu);
+	}
 
 	@Test
 	void testRestore() {
@@ -40,17 +73,93 @@ class TerritoryTest {
 	}
 
 	@Test
-	void testSave() {
+	void testGetTile(){
 		Territory territory = new Territory();
-		territory.changeSize(4, 2);
 		territory.placeAccu(0, 0);
+		territory.placeHollow(1, 0);
+		assertTrue(territory.getTile(0, 0) instanceof Tile);
+		assertTrue(territory.getTile(0, 0).getItem() instanceof Accu);
+		assertTrue(territory.getTile(1, 0) instanceof Hollow);
+	}
 
-		TerritoryState state = territory.save();
+	@Test
+	void testGetNumRows(){
+		Territory territory = new Territory();
+		assertEquals(6, territory.getNumRows());
+		territory.changeSize(4, 2);
+		assertEquals(2, territory.getNumRows());
+	}
 
-		assertNotNull(state);
-		assertEquals(4, state.getNumberOfColumns());
-		assertEquals(2, state.getNumberOfRows());
-		assertTrue(state.getTiles()[0][0].getItem() instanceof Accu);
+	@Test
+	void testGetNumCols(){
+		Territory territory = new Territory();
+		assertEquals(6, territory.getNumCols());
+		territory.changeSize(4, 2);
+		assertEquals(4, territory.getNumCols());
+	}
+
+	@Test
+	void testSetRobbi(){
+		Robbi robbi = new Robbi();
+		
+		Territory territory = new Territory();
+		territory.setRobbi(robbi);
+
+		assertEquals(0, territory.getRobbi().getX());
+		assertEquals(0, territory.getRobbi().getY());
+	}
+
+	@Test
+	void testRobbiOnTile(){
+		Territory territory = new Territory();
+		territory.placeRobbi(2, 3);
+		assertEquals(2, territory.getRobbi().getX());
+		assertEquals(3, territory.getRobbi().getY());
+		assertTrue(territory.robbiOnTile(2, 3));
+	}
+
+	@Test
+	void testPlaceItem(){
+		Territory territory = new Territory();
+		territory.placeItem(new Nut(), 2, 3);
+		assertTrue(territory.getItem(2, 3) instanceof Nut);
+	}
+
+	@Test
+	void testRemoveItem(){
+		Territory territory = new Territory();
+		Nut n = new Nut();
+		territory.placeItem(n, 2, 3);
+		Item i = territory.removeItem(2, 3);
+		assertEquals(n, i);
+		assertFalse(territory.getItem(2, 3) instanceof Nut);
+	}
+
+	@Test
+	void testUpate()throws InvalidTerritoryException{
+
+		Territory otherTerritory= new Territory();
+		otherTerritory.changeSize(4, 5);
+
+		assertEquals(4, otherTerritory.getNumCols());
+		assertEquals(5, otherTerritory.getNumRows());
+		assertEquals(0, otherTerritory.getRobbiX());
+		assertEquals(0, otherTerritory.getRobbiY());
+		assertEquals(null, otherTerritory.getRobbiItem());
+		assertEquals(DIRECTION.EAST, otherTerritory.getRobbiDirection());
+
+		Territory territory = new Territory();
+		Nut n = new Nut();
+		territory.update(otherTerritory, n, 2, 3, DIRECTION.SOUTH);
+		
+		assertEquals(otherTerritory.getNumCols(), territory.getNumCols());
+		assertEquals(otherTerritory.getNumRows(), territory.getNumRows());
+		assertEquals(2, territory.getRobbiX());
+		assertEquals(3, territory.getRobbiY());
+		assertEquals(n, territory.getRobbiItem());
+		assertEquals(DIRECTION.SOUTH, territory.getRobbiDirection());
+		
+		assertThrows(InvalidTerritoryException.class, () -> territory.update(new Territory(), null, 10, 10, DIRECTION.SOUTH));
 	}
 
 	@Test
